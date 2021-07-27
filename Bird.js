d0ef0ae0600af22bc36532ec26e1628beb0c039f
Chirsid <70621611+Chirsid@users.noreply.github.com>
2021-07-27 13:09:02 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,19 @@
+class Bird extends BaseClass {
+    constructor(x, y) {
+      var options = {
+        'density':1.5,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      super(x, y, 50, 50, options);
+      this.image=loadImage("sprites/bird.png")
+      
+    };
+    display(){
+      this.body.position.x=mouseX
+     this.body.position.y=mouseY
+     super.display();
+     
+    };
+  };
+  
